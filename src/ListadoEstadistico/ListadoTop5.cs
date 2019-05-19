0ba2ac17ab4902a02000a2524b29da8fc84cf502
Javier Villalba <jvillalba@fighthawk.com>
2019-05-19 16:50:23 -0300

    ListadoTop5
    LISTADO ESTADISTICOS

---
+++ src/ListadoEstadistico/ListadoTop5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaCrucero.ListadoEstadistico
+{
+    public partial class ListadoTop5 : Form
+    {
+        public ListadoTop5()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
