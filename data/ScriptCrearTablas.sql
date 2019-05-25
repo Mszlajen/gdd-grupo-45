8c1916754d60cb0e10ca544f5c163e77673a0ac5
mszlajen <mszlajen@est.frba.utn.edu.ar>
2019-05-25 10:51:01 -0300

    Constraints y defaults

--- data/ScriptCrearTablas.sql
+++ data/ScriptCrearTablas.sql
@@ -61,185 +61,186 @@ BEGIN
 	--Sentencia crea tabla Usuarios
 	CREATE TABLE MLJ.Usuarios (
 		cod_usuario INTEGER IDENTITY(1,1) PRIMARY KEY,
-		usuario varchar(50),
-		hash_contrasenia char(256),
-		habilitado bit,
-		ingresos_restantes tinyint
+		usuario varchar(50) NOT NULL,
+		hash_contrasenia char(256) NOT NULL,
+		habilitado bit NOT NULL DEFAULT(1),
+		ingresos_restantes tinyint NOT NULL DEFAULT(3)
 	);
 
 	--Sentencia crea tabla UsuariosXRoles
 	CREATE TABLE MLJ.UsuariosXRoles (
-		cod_usuario INTEGER,
-		cod_rol INTEGER
+		cod_usuario INTEGER NOT NULL,
+		cod_rol INTEGER NOT NULL
 	);
 
 	--Sentencia crea tabla Roles
 	CREATE TABLE MLJ.Roles (
 		cod_rol INTEGER IDENTITY(1,1) PRIMARY KEY,
-		descripcion varchar(255),
-		habilitado bit,
-		registrable bit		
+		descripcion varchar(255) NOT NULL,
+		habilitado bit NOT NULL DEFAULT(1),
+		registrable bit NOT NULL DEFAULT(1)		
 	);
 
 	--Sentencia crea tabla RolesXFuncionalidades
 	CREATE TABLE MLJ.RolesXFuncionalidades (
-		cod_rol INTEGER,
-		cod_funcionalidad INTEGER		
+		cod_rol INTEGER NOT NULL,
+		cod_funcionalidad INTEGER NOT NULL		
 	);
 
 	--Sentencia crea tabla Funcionalidades
 	CREATE TABLE MLJ.Funcionalidades (
-		cod_funcionalidad INTEGER IDENTITY(1,1) PRIMARY KEY,
-		descripcion varchar(255)	
+		cod_funcionalidad INTEGER IDENTITY(1,1) PRIMARY KEY NOT NULL,
+		descripcion varchar(255) NOT NULL	
 	);
 
 	--Sentencia crea tabla Viajes
 	CREATE TABLE MLJ.Viajes (
-		cod_viaje INTEGER PRIMARY KEY,
-		fecha_inicio datetime,
+		cod_viaje INTEGER IDENTITY(1,1) PRIMARY KEY,
+		fecha_inicio datetime NOT NULL,
 		fecha_fin_estimada datetime,
-		fecha_fin datetime,
-		cod_recorrido INTEGER,
-		cod_crucero INTEGER,
-		retorna bit,
-		razon_de_cancelacion varchar(255)	
+		fecha_fin datetime NOT NULL,
+		cod_recorrido INTEGER NOT NULL,
+		cod_crucero INTEGER NOT NULL,
+		retorna bit NOT NULL,
+		razon_de_cancelacion varchar(255)
 	);
 
 	--Sentencia crea tabla Recorridos
 	CREATE TABLE MLJ.Recorridos (
-		cod_tramo INTEGER PRIMARY KEY,
-		habilitado bit	
+		cod_recorrido INTEGER IDENTITY(1,1) PRIMARY KEY,
+		habilitado bit NOT NULL DEFAULT(1)
 	);
 
 	--Sentencia crea tabla Tramos
 	CREATE TABLE MLJ.Tramos (
-		cod_recorrido INTEGER,
-		nro_tramo varchar,
-		cod_puerto_salida INTEGER,
-		cod_puerto_llegada INTEGER,
-		costo numeric(18,2),
+		cod_recorrido INTEGER NOT NULL,
+		nro_tramo TINYINT NOT NULL,
+		cod_puerto_salida INTEGER NOT NULL,
+		cod_puerto_llegada INTEGER NOT NULL,
+		costo numeric(18,2) NOT NULL,
 		primary key(cod_recorrido,nro_tramo)
 	);
 
 
 	--Sentencia crea tabla Puertos
 	CREATE TABLE MLJ.Puertos (
-		cod_puerto INTEGER PRIMARY KEY,
-		nombre varchar(255),
-		habilitado bit	
+		cod_puerto INTEGER IDENTITY(1,1) PRIMARY KEY,
+		nombre varchar(255) NOT NULL,
+		habilitado bit NOT NULL DEFAULT(1)	
 	);
 
 	--Sentencia crea tabla Marcas
 	CREATE TABLE MLJ.Marcas (
-		cod_marca INTEGER PRIMARY KEY,
-		nombre varchar(255)
+		cod_marca INTEGER IDENTITY(1,1) PRIMARY KEY,
+		nombre varchar(255) NOT NULL
 	);
 
 	--Sentencia crea tabla Servicios
 	CREATE TABLE MLJ.Servicios (
-		cod_servicio INTEGER PRIMARY KEY,
-		nombre varchar(255)
+		cod_servicio INTEGER IDENTITY(1,1) PRIMARY KEY,
+		nombre varchar(255) NOT NULL
 	);
 
 	--Sentencia crea tabla Fabricantes
 	CREATE TABLE MLJ.Fabricantes (
-		cod_fabricante INTEGER PRIMARY KEY,
-		nombre varchar(255)
+		cod_fabricante INTEGER IDENTITY(1,1) PRIMARY KEY,
+		nombre varchar(255) NOT NULL
 	);
 
 	--Sentencia crea tabla Modelos
 	CREATE TABLE MLJ.Modelos (
-		cod_modelo INTEGER PRIMARY KEY,
-		nombre varchar(255)
+		cod_modelo INTEGER IDENTITY(1,1) PRIMARY KEY,
+		nombre varchar(255) NOT NULL
 	);
 
 	--Sentencia crea tabla Cruceros
 	CREATE TABLE MLJ.Cruceros (
-		cod_crucero INTEGER PRIMARY KEY,
-		identificador varchar(50),
-		fecha_alta datetime,
-		cod_marca INTEGER,	
-		cod_servicio INTEGER,
-		cod_fabricante INTEGER,
-		cod_modelo INTEGER
+		cod_crucero INTEGER IDENTITY(1,1) PRIMARY KEY,
+		identificador varchar(50) NOT NULL,
+		fecha_alta datetime NOT NULL,
+		cod_marca INTEGER NOT NULL,	
+		cod_servicio INTEGER NOT NULL,
+		cod_fabricante INTEGER NOT NULL,
+		cod_modelo INTEGER NOT NULL
 	);
 
 	--Sentencia crea tabla Bajas_de_servicio
 	CREATE TABLE MLJ.Bajas_de_servicio (
-		cod_baja INTEGER PRIMARY KEY,
-		cod_crucero INTEGER,
-		permanente bit,
-		fecha_baja datetime,
+		cod_baja INTEGER IDENTITY(1,1) PRIMARY KEY,
+		cod_crucero INTEGER NOT NULL,
+		permanente bit NOT NULL DEFAULT(0),
+		fecha_baja datetime NOT NULL,
 		fecha_alta datetime
+		CHECK(NOT permanente = 1 OR fecha_alta IS NOT NULL)
 	);
 
 	--Sentencia crea tabla Cabinas
 	CREATE TABLE MLJ.Cabinas (
-		cod_crucero INTEGER,
-		nro decimal(18,0),
-		cod_tipo INTEGER,
-		piso decimal(18,0),
+		cod_crucero INTEGER NOT NULL,
+		nro decimal(18,0) NOT NULL,
+		cod_tipo INTEGER NOT NULL,
+		piso decimal(18,0) NOT NULL,
 		primary key(cod_crucero,nro)
 	);
 
 	--Sentencia crea tabla Tipo_Cabinas
 	CREATE TABLE MLJ.Tipo_Cabinas (
-		cod_tipo INTEGER PRIMARY KEY,
-		valor numeric(4,2),
-		nombre varchar(255)
+		cod_tipo INTEGER IDENTITY(1,1) PRIMARY KEY,
+		valor numeric(4,2) NOT NULL,
+		nombre varchar(255) NOT NULL
 	);
 
 	--Sentencia crea tabla Reservas
 	CREATE TABLE MLJ.Reservas (
-		cod_reserva INTEGER PRIMARY KEY,
-		fecha_reserva datetime,
-		cod_pasaje INTEGER,
+		cod_reserva INTEGER IDENTITY(1,1) PRIMARY KEY,
+		fecha_reserva datetime NOT NULL,
+		cod_pasaje INTEGER NOT NULL,
 		cod_viejo decimal(18,0)
 	);
 
 	--Sentencia crea tabla Pasajes
 	CREATE TABLE MLJ.Pasajes (
-		cod_pasaje INTEGER PRIMARY KEY,
-		cod_cliente INTEGER,
-		cod_viaje INTEGER,
-		cod_pago INTEGER,
-		cantidad numeric(18,2)
+		cod_pasaje INTEGER IDENTITY(1,1) PRIMARY KEY,
+		cod_cliente INTEGER NOT NULL,
+		cod_viaje INTEGER NOT NULL,
+		cod_pago INTEGER NOT NULL,
+		cantidad numeric(18,2) NOT NULL
 	);
 
 	--Sentencia crea tabla Cabinas_reservadas
 	CREATE TABLE MLJ.Cabinas_reservadas (
-		cod_pasaje INTEGER,
-		cod_crucero INTEGER,
-		nro_cabina decimal(18,0),
+		cod_pasaje INTEGER NOT NULL,
+		cod_crucero INTEGER NOT NULL,
+		nro_cabina decimal(18,0) NOT NULL,
 		primary key(cod_pasaje,cod_crucero,nro_cabina)
 	);
 
 	--Sentencia crea tabla Pagos
 	CREATE TABLE MLJ.Pagos (
-		cod_pago INTEGER PRIMARY KEY,
-		fecha datetime,
-		cod_medio INTEGER,
-		hash_nro_tarjeta char(255),
-		ultimos_digitos char(4),
-		cod_seguridad char(4)
+		cod_pago INTEGER IDENTITY(1,1) PRIMARY KEY,
+		fecha datetime NOT NULL,
+		cod_medio INTEGER NOT NULL,
+		hash_nro_tarjeta char(255) NOT NULL,
+		ultimos_digitos char(4) NOT NULL,
+		cod_seguridad char(4) NOT NULL
 	);
 
 	--Sentencia crea tabla Medios_de_Pago
 	CREATE TABLE MLJ.Medios_de_Pago (
-		cod_medio INTEGER PRIMARY KEY,
-		nombre varchar(255)
+		cod_medio INTEGER IDENTITY(1,1) PRIMARY KEY,
+		nombre varchar(255) NOT NULL
 	);
 
 	--Sentencia crea tabla Clientes
 	CREATE TABLE MLJ.Clientes (
-		cod_cliente INTEGER PRIMARY KEY,
-		nombre varchar(255),
-		apellido varchar(255),
-		dni decimal(18,0),
-		direccion varchar(255),
-		telefono INTEGER,
+		cod_cliente INTEGER IDENTITY(1,1) PRIMARY KEY,
+		nombre varchar(255) NOT NULL,
+		apellido varchar(255) NOT NULL,
+		dni decimal(18,0) NOT NULL,
+		direccion varchar(255) NOT NULL,
+		telefono INTEGER NOT NULL,
 		mail varchar(255),
-		nacimiento datetime
+		nacimiento datetime NOT NULL
 	
 	);
 
